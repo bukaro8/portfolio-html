570b432159cca6439e9b218b2c7baac3dd57f1f8
Victor Ramirez <vicdeveloper83@gmail.com>
2025-01-29 17:47:51 +0000

    projects cards finnished

---
+++ js/modals.js
@@ -0,0 +1,31 @@
+document.addEventListener('DOMContentLoaded', function () {
+	function openModal(modalId) {
+		const modal = document.getElementById(modalId);
+		if (modal) {
+			modal.classList.remove('hidden');
+			modal.classList.add('flex');
+		}
+	}
+
+	function closeModal(modalId) {
+		const modal = document.getElementById(modalId);
+		if (modal) {
+			modal.classList.add('hidden');
+			modal.classList.remove('flex');
+		}
+	}
+
+	document.querySelectorAll('[data-modal-toggle]').forEach((button) => {
+		button.addEventListener('click', function () {
+			const targetModal = button.getAttribute('data-modal-target');
+			openModal(targetModal);
+		});
+	});
+
+	document.querySelectorAll('[data-modal-hide]').forEach((closeButton) => {
+		closeButton.addEventListener('click', function () {
+			const targetModal = closeButton.getAttribute('data-modal-hide');
+			closeModal(targetModal);
+		});
+	});
+});
